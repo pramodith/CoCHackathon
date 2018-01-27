0abd2dbb34da679b37ae52c224dd6e7cf67f4eae
pramodith <pramodith1@gmail.com>
2018-01-27 17:31:48 -0500

    API code to get recipes,identify the food.
    Signed-off-by: pramodith <pramodith1@gmail.com>

---
+++ food_bank_api.py
@@ -0,0 +1,42 @@
+import requests
+import urllib.parse
+import json
+import pprint
+class Food_Bank:
+
+    def __init__(self):
+        self.api_id_recipe="af08e16d"
+        self.api_key_recipe="00c8c5a5d4723815822edcfd56de9dd4"
+        self.api_id_food="5140730c"
+        self.api_key_food="b5e3d17b41efb6e533827f95e9ab3151"
+
+    def get_food(self,text):
+        url_text=urllib.parse.quote_plus(text)
+        response=requests.get('https://api.edamam.com/api/food-database/parser?ingr=='+url_text+'&app_id='+self.api_id_food+'&app_key='+self.api_key_food+'&page=0')
+        food=(response.json())
+        pp = pprint.PrettyPrinter(width=10, compact=True)
+        food_items=food['hints'][0]['food']['label']
+        return food_items
+
+    def fill_food(self,text):
+        url_text=urllib.parse.quote_plus(text)
+        response=requests.get("https://api.edamam.com/auto-complete?q ="+url_text+"&limit=10&app_id =$"+self.api_id_food+"&app_key =$"+self.api_key_food)
+        suggested=response.json()
+        print(suggested)
+
+    def get_recipe(self,food_items):
+        recipes=[]
+        url_text = urllib.parse.quote_plus(food_items)
+        response = requests.get('https://api.edamam.com/search?q='+url_text+'&app_id='+self.api_id_recipe+'&app_key='+self.api_key_recipe+'&from=0&to=3')
+        food = (response.json())
+        food=food['hits']
+        print(food[1])
+        for i in range(3):
+            recipes.append(food[i]['recipe']['label']+": ")
+            for ingredient in food[i]['recipe']['ingredients']:
+                recipes[i]+=ingredient['text']+", "
+            recipes[i]+="\n"+food[i]['recipe']['url']
+        return recipes
+
+f=Food_Bank()
+print(f.get_recipe("chicken,milk"))
